ba072bc68170e6a91f260593264b0a6bf9387831
dviveiros <daniel.viveiros@gmail.com>
2015-06-23 20:41:07 -0300

    Edit text working

---
+++ app/src/main/java/com/danielviveiros/spotifystreamer/FetchArtistsTask.java
@@ -0,0 +1,7 @@
+package com.danielviveiros.spotifystreamer;
+
+/**
+ * Created by dviveiros on 23/06/15.
+ */
+public class FetchArtistsTask {
+}
--- app/src/main/java/com/danielviveiros/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/danielviveiros/spotifystreamer/MainActivityFragment.java
@@ -2,9 +2,17 @@ package com.danielviveiros.spotifystreamer;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -12,12 +20,85 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    /** Log tag */
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    /** Adapter to deal with the list of artists */
+    private ArrayAdapter<String> mArtistListAdapter;
+
+    /** List view */
+    private ListView mForecastListView;
+
+    /** Filter */
+    private String mArtistFilter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //set the listener for the edit text
+        EditText artistEditText = (EditText) rootView.findViewById(R.id.artist_filter);
+
+        //prepare the list view and adapter
+        mArtistListAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.artist_textview,R.id.artist_textview);
+        mForecastListView = (ListView) rootView.findViewById(R.id.listview_artist);
+        mForecastListView.setAdapter(mArtistListAdapter);
+
+        //configure the listener for the artist text field
+        artistEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    mArtistFilter = v.getText().toString();
+                    updateArtistList();
+                    return true;
+                }
+                return false;
+                /*
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    if (!event.isShiftPressed()) {
+                        // the user is done typing.
+                        mArtistFilter = v.getText().toString();
+                        Log.v(LOG_TAG, "Artist filter = " + mArtistFilter);
+                        updateArtistList();
+                        return true; // consume.
+                    }
+                }
+                return false; // pass on to other listeners.
+                */
+            }
+        });
+
+
+
+        return rootView;
+    }
+
+    /**
+     * Updates the list of artists shown
+     */
+    private void updateArtistList() {
+
+        //hides the keyboard
+        InputMethodManager imm = (InputMethodManager)
+                this.getActivity().getSystemService(this.getActivity().
+                        getApplicationContext().INPUT_METHOD_SERVICE);
+        if(imm != null){
+            imm.toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }
+
+        //show the result
+        Toast toast = Toast.makeText(this.getActivity().getBaseContext(), mArtistFilter, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
