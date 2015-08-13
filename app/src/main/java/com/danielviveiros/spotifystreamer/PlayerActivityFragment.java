599bedbd04a361d18246062a586996e734941510
Daniel Viveiros <daniel.viveiros@gmail.com>
2015-08-13 20:51:18 -0300

    Creating the player UI

---
+++ app/src/main/java/com/danielviveiros/spotifystreamer/PlayerActivityFragment.java
@@ -0,0 +1,22 @@
+package com.danielviveiros.spotifystreamer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlayerActivityFragment extends Fragment {
+
+    public PlayerActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.player_fragment, container, false);
+    }
+}
